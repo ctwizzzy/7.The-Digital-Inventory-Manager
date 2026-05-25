faaa0a7b532f96bee157493d9d382f0c31fb6505
ctwizzy <aaravrajkumar852@gmail.com>
2026-05-25 10:58:29 +1000

    Create main.py
    made updates on main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+class product: 
+    def __init__(self, product_id, name, price, quantity)
+     self.product_id = product_id
+     self.name = name
+     self.price = price
+     self.quantity = quantity 
+
+def update_stock(self, amount):
+    self.amount = amount
+    return amount 
+    
+   
+    
+def get_total_value(self, price, quantity):            
+    value = quantity * price
+    return value        
+                    
