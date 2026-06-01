2f0f5f0f0b0701bb4e2841705b43592840cabd23
ctwizzy <aaravrajkumar852@gmail.com>
2026-06-01 15:09:09 +1000

    updates on main.py
    Made updates on my main.py file

--- main.py
+++ main.py
@@ -1,34 +1,46 @@
 class product: 
-    def __init__(self, product_id, name, price, quantity, amount):
+    def __init__(self, product_id, name, price, quantity):
      self.product_id = product_id
      self.name = name
      self.price = price
      self.quantity = quantity 
-     self.amount = amount
 
 def add_stock(self, amount):
+    self.amount = amount
     return amount + 1
     
 
 def remove_stock(self, amount):
+
     if amount == -1:
         print("Unavailable, stock cannot be negative")
 
+   
        
 def get_total_value(self, price, amount):            
     value = amount * price
-    return value  
-
+    return value      
 
 def restock(self, product_id, amount):
     self.product_id = product_id
     if amount == 0:
         print("Out of stock, please restock")     
 
-
 def display_all(self, product_id, name, price, amount):
     print("Product ID: ", product_id)
     print("Name: ", name)
     print("Price: ", price)
-    print("Amount: ", amount)     
-                    
+    print("Amount: ", amount) 
+
+class Inventory:
+    def __init__(self): 
+        self.products = {}    
+
+def add_product(self, product):
+    self.products = product{product.product_id} = product
+
+def restock(self, product_id, amount):
+    self.product_id = product_id
+    if amount == 0:
+        print("Out of stock, please restock")  
+    
