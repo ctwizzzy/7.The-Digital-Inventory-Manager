da6bf5d9a3e133efd8cbda7a5cb0315a37463684
ctwizzy <aaravrajkumar852@gmail.com>
2026-06-03 10:11:00 +1000

    Update main.py
    Fixed up my attributes as they were mixed up in the classes with the wrong attributes being in the wrong classes. This has been fixed now and all attributes and methods are in the correct classes.

--- main.py
+++ main.py
@@ -5,18 +5,26 @@ class product:
      self.price = price
      self.quantity = quantity 
 
-def add_stock(self, amount):
+def add_stock(self, amount, quantity):
     self.amount = amount
-    return amount + 1
+    quantity = int(input("How much stock would you like to add?" ))
+    amount = amount + quantity  
     
 
-def remove_stock(self, amount):
+def remove_stock(self, amount, quantity):
+    quantity = int(input("How much stock would you like to remove?" ))
+    amount = amount - quantity 
 
-    if amount == -1:
-        print("Unavailable, stock cannot be negative")
+def __str__(self): 
+    return f"{self.product_id} | {self.name} | {self.price} | {self.quantity}"
 
-   
-       
+
+class Inventory:   
+
+def __init__(self, product):
+    self.product = product
+
+
 def get_total_value(self, price, amount):            
     value = amount * price
     return value      
@@ -32,15 +40,4 @@ def display_all(self, product_id, name, price, amount):
     print("Price: ", price)
     print("Amount: ", amount) 
 
-class Inventory:
-    def __init__(self): 
-        self.products = {}    
 
-def add_product(self, product):
-    self.products = product{product.product_id} = product
-
-def restock(self, product_id, amount):
-    self.product_id = product_id
-    if amount == 0:
-        print("Out of stock, please restock")  
-    
