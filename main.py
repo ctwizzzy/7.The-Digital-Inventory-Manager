b0af09ab43ebd7464a5ab1ebc43808cce7381057
ctwizzy <aaravrajkumar852@gmail.com>
2026-06-03 10:11:44 +1000

    Update main.py
    updated main.py

--- main.py
+++ main.py
@@ -19,11 +19,9 @@ def __str__(self):
     return f"{self.product_id} | {self.name} | {self.price} | {self.quantity}"
 
 
-class Inventory:   
-
-def __init__(self, product):
-    self.product = product
-
+class Inventory:
+    def __init__(self, product):
+        self.product = product
 
 def get_total_value(self, price, amount):            
     value = amount * price
@@ -34,10 +32,17 @@ def restock(self, product_id, amount):
     if amount == 0:
         print("Out of stock, please restock")     
 
-def display_all(self, product_id, name, price, amount):
+def display_all(self, product_id, name, price, quantity):
+    f"{self.product_id} | {self.name} | {self.price} | {self.quantity}"
     print("Product ID: ", product_id)
     print("Name: ", name)
     print("Price: ", price)
-    print("Amount: ", amount) 
+    print("Quantity: ", quantity) 
 
-
+if __name__ == "__main__":
+   print("Welcome to the Digital Inventory Manager")
+while True:
+    print("1. Add New Product")
+    print("2. View All Inventory")
+    print("3. Update Product Stock (Buy/Sell)")
+    print("4. Exit")
